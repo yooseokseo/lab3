7cc40a98366372b562ff17cf4ffa16d5f6313c8b
yooseok Seo <yos017@ucsd.edu>
2018-01-25 00:26:45 -0800

    changed css and js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,32 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(this).text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
+
 	});
+	$("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+		
+    if (description.length == 0) {
+
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+
+
+
+
+
+    } else {
+
+			  $(".project-description").fadeOut();
+    }
 }
